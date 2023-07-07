f1f7cb916eee5879f519285ad38d1068f9047299
StephenMal <Maldonado527@gmail.com>
2023-07-06 22:46:18 -0400

    rebuilt

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230706.223327';
+const cacheName = 'chirpy-20230706.224553';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
